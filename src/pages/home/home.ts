ff736ed27937ef71c950e3aef8c3c3bedc51e9f9
deepakvashishth <deepakabacusdesk@gmail.com>
2023-03-16 19:05:07 +0530

    pan commnt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -622,7 +622,7 @@ export class HomePage {
         let image = "";
         let app_url = "https://play.google.com/store/apps/details?id=com.bonsonelectricals.app";
         
-        this.socialSharing.share("Hey there join me (" + this.karigar_detail.full_name + "-" + this.karigar_detail.mobile_no + ") on Veto Switchgears , a Electrician & Dealer app. Enter my code *" + this.karigar_detail.referral_code + "* to earn points back in your wallet!", "Reffral", image, app_url)
+        this.socialSharing.share("Hey there join me (" + this.karigar_detail.full_name + "-" + this.karigar_detail.mobile_no + ") on Bonson Electricals , a Electrician app. Enter my code *" + this.karigar_detail.referral_code + "* to earn points back in your wallet!", "Reffral", image, app_url)
         .then(resp=>{
             console.log(resp,'called');
             
